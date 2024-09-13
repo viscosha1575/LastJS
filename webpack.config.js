92ac9ea1616596c34a3778c09a5097c08bbaff71
viscosha1575 <61380433+viscosha1575@users.noreply.github.com>
2024-09-13 03:53:48 +0200

    ggg

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // Убедитесь, что mode установлен
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            context: path.resolve(__dirname, 'src'),
+                            outputPath: 'assets/', // Вывод файлов в папку assets
+                            publicPath: 'assets/',
+                            useRelativePaths: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        compress: true,
+        port: 9000,
+        hot: true
+    }
+};
